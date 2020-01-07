6e482a2be9cc4ebc248c9d09d5f202cf811a0ebf
albino.simonetti <a.simonetti@reply.it>
2020-01-07 14:50:19 +0100

    add exception

---
+++ src/main/java/it/reply/cof/utils/COFException.java
@@ -0,0 +1,20 @@
+package it.reply.cof.utils;
+
+public class COFException extends Exception{
+
+    private final String message;
+    private final Throwable rootCause;
+
+    public COFException(String message, Throwable rootCause) {
+        this.message = message;
+        this.rootCause = rootCause;
+    }
+
+    public String getExceptionMessage() {
+        return message;
+    }
+
+    public Throwable getRootCause() {
+        return rootCause;
+    }
+}
