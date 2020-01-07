6218d4929693a41a8e1a7f2937169c5d85c31b4f
Gabriel Raul Marini <gab.marini@reply.it>
2020-01-07 14:47:41 +0100

    Created VPOS client interface with basic methods

---
+++ src/main/java/it/reply/cof/client/VPOSClient.java
@@ -0,0 +1,20 @@
+package it.reply.cof.client;
+
+import it.reply.cof.dto.PaymentInfo;
+
+/**
+ * VPOS Client interface used to perform common requests and validate returned results from SIA.
+ * Different implementations are provided to offer a full spectre of tools fitting security and
+ * and common scenarios requirements.
+ *
+ * @author gab.marini
+ */
+public interface VPOSClient {
+
+    void injectMasterTemplate();
+
+    String getPaymentHtmlDocument(PaymentInfo paymentInfo);
+
+    void verifyResponse() throws COFException;
+
+}
---
+++ src/main/java/it/reply/cof/dto/PaymentInfo.java
@@ -0,0 +1,4 @@
+package it.reply.cof.dto;
+
+public class PaymentInfo {
+}
