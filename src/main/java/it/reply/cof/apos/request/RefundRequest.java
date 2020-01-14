2484c785390ca1e115f356cc9c6a0985b069c80f
Gabriel Raul Marini <gab.marini@reply.it>
2020-01-14 12:28:35 +0100

    Refactoring or refund and confirm request

--- src/main/java/it/reply/cof/apos/request/RefundRequest.java
+++ src/main/java/it/reply/cof/apos/request/RefundRequest.java
@@ -6,19 +6,8 @@ import javax.xml.bind.annotation.XmlElement;
 import java.util.Date;
 
 @XmlAccessorType(XmlAccessType.FIELD)
-public class RefundRequest {
+public class RefundRequest extends GeneralRequest {
 
-    @XmlElement(name = "Header")
-    private Header header;
-    @XmlElement(name = "TransactionID")
-    private String transactionId;
-    @XmlElement(name = "OrderID")
-    private String orderId;
-    @XmlElement(name = "Amount")
-    private String amount;
-    @XmlElement(name = "Currency")
-    private String currency;
-    @XmlElement(name = "Exponent")
     private String exponent;
     @XmlElement(name = "OpDescr")
     private String opDescr;
@@ -26,55 +15,7 @@ public class RefundRequest {
     private String options;
 
     public RefundRequest(Date reqDate) {
-        this.header = new Header(reqDate);
-    }
-
-    public Header getHeader() {
-        return header;
-    }
-
-    public void setHeader(Header header) {
-        this.header = header;
-    }
-
-    public String getTransactionId() {
-        return transactionId;
-    }
-
-    public void setTransactionId(String transactionId) {
-        this.transactionId = transactionId;
-    }
-
-    public String getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(String orderId) {
-        this.orderId = orderId;
-    }
-
-    public String getAmount() {
-        return amount;
-    }
-
-    public void setAmount(String amount) {
-        this.amount = amount;
-    }
-
-    public String getCurrency() {
-        return currency;
-    }
-
-    public void setCurrency(String currency) {
-        this.currency = currency;
-    }
-
-    public String getExponent() {
-        return exponent;
-    }
-
-    public void setExponent(String exponent) {
-        this.exponent = exponent;
+        super(reqDate);
     }
 
     public String getOpDescr() {
