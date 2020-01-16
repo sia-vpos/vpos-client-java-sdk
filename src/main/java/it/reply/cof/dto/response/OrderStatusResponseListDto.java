3271ed4a07834145def4ab88a34d29727344f158
dcolciaghi <47253562+9Dxn@users.noreply.github.com>
2020-01-16 14:42:14 +0100

    mmm

--- src/main/java/it/reply/cof/dto/response/OrderStatusResponseListDto.java
+++ src/main/java/it/reply/cof/dto/response/OrderStatusResponseListDto.java
@@ -8,5 +8,5 @@ public class OrderStatusResponseListDto {
    public List<OrderStatusResponseDto> oSRElements = new ArrayList<>();
    public OrderStatusResponseDto oSRCommon = new OrderStatusResponseDto();
 
-   
+
 }
